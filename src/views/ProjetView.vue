8fd94f6ecab7566c059597b78cb0a1513d4cbb45
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-02-28 20:23:43 +0100

    ProjetView.vue : importation des element de firebase + debut de mise en forme

--- src/views/ProjetView.vue
+++ src/views/ProjetView.vue
@@ -1,10 +1,57 @@
 <template>
-    <main class="px-7 md:px-10 pt-24">
-        <h1>{{ projet.titre }}</h1>
+    <main class="pt-24">
+        <h1 class="px-7 md:px-10">{{ projet.titre }}</h1>
 
-        <img :src="img_rect" alt="image noon trouver">
+        <div class="my-16 w-full md:absolute md:right-0 md:m-0 md:w-1/2">
+            <div class="projet_img relative z-10">
+                <img :src="img_rect" alt="image non trouver">
+            </div>
+    
+            <a target="_blank" :href="projet.lien">
+                <monBouton class="mx-auto my-16">
+                    Voir le Projet
+                </monBouton>
+            </a>
+        </div>
+
+        <div class="px-7 md:px-10">
+            <p class="my-8 font-oswald font-bold text-lg text-orange-100">Année :<span class="ml-5 font-darker-grotesque text-base text-stone-300">{{ projet.annee }}</span></p>
+    
+            <p class="my-8 font-oswald font-bold text-lg text-orange-100">Type : <span class="ml-5 font-darker-grotesque text-base text-stone-300">{{ projet.type }}</span></p>
+    
+            <p class="my-8 font-oswald font-bold text-lg text-orange-100">Contex <span class="ml-5 font-darker-grotesque text-base text-stone-300">{{ projet.contexte_all }}</span></p>
+        </div>
+
+
     </main>
 </template>
+
+<style>
+
+.projet_img::before{
+    content: "";
+    position: absolute;
+    z-index: -1;
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: -4%;
+    left: -2%;
+    background-color: #22d3ee;
+}
+.projet_img::after{
+    content: "";
+    position: absolute;
+    z-index: -1;
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 4%;
+    left: 2%;
+    background-color: #d946ef;
+}
+
+</style>
 
 
 <script>
@@ -46,10 +93,15 @@ export default {
         return {
             imgData: null,
             projet:{
-                titre: null,
+                anne: null,
+                contexte_all: null,
                 image_rect: null,
+                lien: null,
+                titre: null,
+                type: null, 
+                contexte_all_test: [],
             },
-
+            
             refProjet:null,
             img_rect:null 
         }
@@ -82,6 +134,7 @@ export default {
             .catch((error) =>{
                 console.log('erreur downloadUrl', error);
             })
+
         },
     }
 }
