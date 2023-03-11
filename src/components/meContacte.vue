c7650713cd1a2d04424b6e02ea18456faf932226
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-03-11 01:35:05 +0100

    meContacte.vue : mise en ligne des élements au dessus d'une certaine taille + sticky sur les réseaux sociaux

--- src/components/meContacte.vue
+++ src/components/meContacte.vue
@@ -1,131 +1,146 @@
 <template>
-    <section class="my-28">
+    <section class="py-28">
         <h2>Me Contacter</h2>
 
-        <div class="mx-5 my-8 md:mx-14 md:my-16 xl:mx-28 flex md:block flex-wrap justify-between gap-5">
+        <!--mise en ligne des élements (formulaire + réseaux) en ligne-->
+        <div class="lg:flex flex-row-reverse">
 
-            <div class="md:my-8 w-full max-w-2xl flex flex-wrap flex-row justify-between gap-5 md:gap-8 ">
-                <a target="_blank" href="https://www.linkedin.com/in/aurélie-runser-10a763224">
+            <!-- comportement des 2 bloques des réseaux -->
+            <div class="mx-5 my-8 md:mx-14 md:my-16 xl:mx-28 flex md:block flex-wrap justify-between gap-5">
     
-                    <linkedinIcon class="inline-block w-7 h-7 md:w-10 md:h-10"/>
-                    <p class="sr-only">Mon compte Linkedin</p>
-                    <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-cyan-400">
-                        /in/aurélie-runser-10a763224
-                    </p>
-    
-                </a>
-    
-                <a target="_blank" href="https://www.instagram.com/aurelie.projetmmi/">
-    
-                    <instagramIcon class="inline-block w-7 h-7 md:w-10 md:h-10"/>
-                    <p class="sr-only">Mon compte Instagram</p>
-                    <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-fuchsia-500">
-                        @aurelie.projetmmi
-                    </p>
-    
-                </a>
-            </div>
+                <!-- sticky -->
+                <div class="sticky top-1/3">
 
+                    <!-- bloque linkedin + insta-->
+                    <div class=" my-8 w-full max-w-2xl flex flex-wrap flex-row justify-between lg:flex-col gap-5 md:gap-8 ">
 
-            <div class="md:my-8 w-full max-w-2xl flex flex-wrap flex-row justify-between gap-5 md:gap-8 ">
-                <a href="mailto:aurelie.runser25@gmail.com">
-    
-                    <mailIcon class="inline-block w-9 h-9 md:w-12 md:h-12"/>
-                    <p class="sr-only">Mon Adresse Mail</p>
-                    <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-cyan-400">
-                        aurelie.runser25@gmail.com
-                    </p>
-    
-                </a>
-    
-                <a href="tel:+33771722003">
-    
-                    <phonIcon class="inline-block w-9 h-9 md:w-12 md:h-12"/>
-                    <p class="sr-only">Mon Numéro de Téléphone</p>
-                    <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-fuchsia-500">
-                        +33 7 71 72 20 03
-                    </p>
-    
-                </a>
-            </div>
+                        <a target="_blank" href="https://www.linkedin.com/in/aurélie-runser-10a763224">
+            
+                            <linkedinIcon class="inline-block w-7 h-7 md:w-10 md:h-10"/>
+                            <p class="sr-only">Mon compte Linkedin</p>
+                            <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-cyan-400">
+                                /in/aurélie-runser-10a763224
+                            </p>
+            
+                        </a>
+            
+                        <a target="_blank" href="https://www.instagram.com/aurelie.projetmmi/">
+            
+                            <instagramIcon class="inline-block w-7 h-7 md:w-10 md:h-10"/>
+                            <p class="sr-only">Mon compte Instagram</p>
+                            <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-cyan-400">
+                                @aurelie.projetmmi
+                            </p>
+            
+                        </a>
 
-        </div>
+                    </div>
         
-        <form action="https://api.web3forms.com/submit" method="POST" ref="contactForm"
-            class="mx-5 my-14 md:mx-14 md:my-20 md:max-w-2xl xl:mx-28">
-
-
-            <input type="hidden" name="access_key" value="b3a5c0b1-dcc2-48e8-90e1-8ce766ba6253">
-
-            <div class="relative my-16">
-                <input type="text" name="name" placeholder=" " required
-                        class="border-2 border-orange-100 p-2 md:p-5">
-                <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
-                        font-darker-grotesque font-medium  text-orange-100 opacity-70">
-                    Prénom & Nom
-                </p>
-
-                <div class="form_obligatoire p-3 md:p-5">
-                    <p class="font-darker-grotesque font-bold text-sm 
-                                md:text-base">* Obligatoire</p>
-                </div>
-            </div>
-
-            <div class="relative my-16">
-                <input type="email" name="email" placeholder=" " required
-                        class="border-2 border-orange-100 p-2 md:p-5">
-                <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
-                        font-darker-grotesque font-medium  text-orange-100 opacity-70">
-                    Adresse Mail
-                </p>
-
-                <div class="form_obligatoire p-3 md:p-5">
-                    <p class="font-darker-grotesque font-bold text-sm 
-                                md:text-base">* Obligatoire</p>
+                    <!-- bloque mail + phone -->
+                    <div class=" md:my-8 w-full max-w-2xl flex flex-wrap flex-row justify-between lg:flex-col gap-5 md:gap-8 ">
+                        <a href="mailto:aurelie.runser25@gmail.com">
+            
+                            <mailIcon class="inline-block w-9 h-9 md:w-12 md:h-12"/>
+                            <p class="sr-only">Mon Adresse Mail</p>
+                            <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-fuchsia-500">
+                                aurelie.runser25@gmail.com
+                            </p>
+            
+                        </a>
+            
+                        <a href="tel:+33771722003">
+            
+                            <phonIcon class="inline-block w-9 h-9 md:w-12 md:h-12"/>
+                            <p class="sr-only">Mon Numéro de Téléphone</p>
+                            <p class="inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 hover:text-fuchsia-500">
+                                +33 7 71 72 20 03
+                            </p>
+            
+                        </a>
+                    </div>
+        
                 </div>
             </div>
             
-            <div class="relative my-16">
-                <input type="text" name="sujet" placeholder=" " required
-                        class="border-2 border-orange-100 p-2 md:p-5">
-                <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
-                        font-darker-grotesque font-medium  text-orange-100 opacity-70">
-                    Sujet
+            <!-- formulaire-->
+            <div class="grow">
+                <form action="https://api.web3forms.com/submit" method="POST" ref="contactForm"
+                    class="mx-5 my-14 md:mx-14 md:my-20 md:max-w-2xl xl:mx-28">
+        
+        
+                    <input type="hidden" name="access_key" value="b3a5c0b1-dcc2-48e8-90e1-8ce766ba6253">
+        
+                    <div class="relative my-16">
+                        <input type="text" name="name" placeholder=" " required
+                                class="border-2 border-orange-100 p-2 md:p-5">
+                        <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
+                                font-darker-grotesque font-medium  text-orange-100 opacity-70">
+                            Prénom & Nom
+                        </p>
+        
+                        <div class="form_obligatoire p-3 md:p-5">
+                            <p class="font-darker-grotesque font-bold text-sm 
+                                        md:text-base">* Obligatoire</p>
+                        </div>
+                    </div>
+        
+                    <div class="relative my-16">
+                        <input type="email" name="email" placeholder=" " required
+                                class="border-2 border-orange-100 p-2 md:p-5">
+                        <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
+                                font-darker-grotesque font-medium  text-orange-100 opacity-70">
+                            Adresse Mail
+                        </p>
+        
+                        <div class="form_obligatoire p-3 md:p-5">
+                            <p class="font-darker-grotesque font-bold text-sm 
+                                        md:text-base">* Obligatoire</p>
+                        </div>
+                    </div>
+                    
+                    <div class="relative my-16">
+                        <input type="text" name="sujet" placeholder=" " required
+                                class="border-2 border-orange-100 p-2 md:p-5">
+                        <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
+                                font-darker-grotesque font-medium  text-orange-100 opacity-70">
+                            Sujet
+                        </p>
+        
+                        <div class="form_obligatoire p-3 md:p-5">
+                            <p class="font-darker-grotesque font-bold text-sm 
+                                        md:text-base">* Obligatoire</p>
+                        </div>
+                    </div>
+        
+                    <div class="relative my-16">
+                        <textarea name="message" rows="5" placeholder=" " required
+                        class="border-2 border-orange-100 p-2 md:p-5"></textarea>
+                        <p class="form_text absolute top-2 left-3
+                                font-darker-grotesque font-medium text-lg text-orange-100 opacity-70
+                                md:text-xl">
+                            Message
+                        </p>
+        
+                        <div class="form_obligatoire p-3 md:p-5">
+                            <p class="font-darker-grotesque font-bold text-sm 
+                                        md:text-base">* Obligatoire</p>
+                        </div>
+                    </div>
+        
+                    <!-- <input type="hidden" name="redirect" value="https://web3forms.com/success"> -->
+        
+                    <monBouton type="submit" class="mx-auto">
+                        Envoyer
+                    </monBouton>
+        
+                </form>
+        
+                <p v-if="messageSent"
+                    class="form_messageSent relative mx-auto w-fit font-oswald font-bold text-lg xl:text-xl text-center text-stone-300">
+                    Merci beaucoup, votre message a bien été envoyé !
                 </p>
-
-                <div class="form_obligatoire p-3 md:p-5">
-                    <p class="font-darker-grotesque font-bold text-sm 
-                                md:text-base">* Obligatoire</p>
-                </div>
             </div>
-
-            <div class="relative my-16">
-                <textarea name="message" rows="5" placeholder=" " required
-                class="border-2 border-orange-100 p-2 md:p-5"></textarea>
-                <p class="form_text absolute top-2 left-3
-                        font-darker-grotesque font-medium text-lg text-orange-100 opacity-70
-                        md:text-xl">
-                    Message
-                </p>
-
-                <div class="form_obligatoire p-3 md:p-5">
-                    <p class="font-darker-grotesque font-bold text-sm 
-                                md:text-base">* Obligatoire</p>
-                </div>
-            </div>
-
-            <!-- <input type="hidden" name="redirect" value="https://web3forms.com/success"> -->
-
-            <monBouton type="submit" class="mx-auto">
-                Envoyer
-            </monBouton>
-
-        </form>
-
-        <p v-if="messageSent"
-            class="form_messageSent relative mx-auto w-fit font-oswald font-bold text-lg xl:text-xl text-center text-stone-300">
-            Merci beaucoup, votre message a bien été envoyé !
-        </p>
+        </div>
 
     </section>
 </template>
