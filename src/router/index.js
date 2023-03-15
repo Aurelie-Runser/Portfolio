d10b4597b6f72fb22cb232e4f8be7a0262b2f971
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-03-15 08:56:30 +0100

    index.js & 404.vue : ajout d'une page 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjetView from '../views/ProjetView.vue'
+import Page404View from '../views/404.vue'
 
 
 const router = createRouter({
@@ -8,6 +9,7 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/projet/:id', name: 'projet', component: ProjetView },
+    { path: "/:catchAll(.*)", name: "Page404View", component: Page404View,}, 
   ],
 
   scrollBehavior() {
