46c6b8dfae1e3120d918d05119da2278ceebfff1
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-05-01 17:20:55 +0200

    ajout d'un lien vers mon compte github + animation couleur au hover sur les liens refait

--- src/components/monFooter.vue
+++ src/components/monFooter.vue
@@ -19,7 +19,12 @@
     
             <a target="_blank" href="https://www.instagram.com/aurelie.runser_pro/">
                 <instagramIconGray class="inline-block w-5 h-5 md:w-8 md:h-8"/>
-                <p class="sr-only">Lien vers mon compte Instagram : </p>
+                <p class="sr-only">Lien vers mon compte Instagram</p>
+            </a>
+            
+            <a target="_blank" href="https://github.com/Aurelie-Runser">
+                <githubIconGray class="inline-block w-5 h-5 md:w-8 md:h-8"/>
+                <p class="sr-only">Lien vers mon compte Github</p>
             </a>
         </div>
 
@@ -36,24 +41,26 @@
 </template>
 
 <style scoped>
-/* animation du underline */
+/* animation du underline des liens */
 .footer_link:hover{
-    animation: underline_color 0.5s linear infinite;
+    animation: underline_color 1s linear infinite;
 }
 
 @keyframes underline_color {
-    from{text-decoration-color: #22d3ee;}
-    to{text-decoration-color: #d946ef;}
+    0%{text-decoration-color: #d946ef;}
+    50%{text-decoration-color: #22d3ee;}
+    100%{text-decoration-color: #d946ef;}
 }
 </style>
 
 <script>
 import linkedinIconGray from "@/components/icons/linkedinIconGray.vue"
 import instagramIconGray from "@/components/icons/instagramIconGray.vue"
+import githubIconGray from "@/components/icons/githubIconGray.vue"
 
 export default {
   name: 'ContactForm',
-  components: {linkedinIconGray, instagramIconGray},
+  components: {linkedinIconGray, instagramIconGray, githubIconGray},
 }
 
 </script>
