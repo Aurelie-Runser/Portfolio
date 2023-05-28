d6e434fa197f724d501f460f22a80e4dc300b55c
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-05-28 18:07:48 +0200

    meContacte.vue : ajout regles RGPD

--- src/components/meContacte.vue
+++ src/components/meContacte.vue
@@ -14,12 +14,12 @@
                     <!-- bloque linkedin + insta-->
                     <div class="my-8 w-full flex flex-wrap flex-row justify-between xl:flex-col gap-5 md:gap-8 ">
 
-                        <a target="_blank" href="https://www.linkedin.com/in/aurélie-runser-10a763224">
+                        <a target="_blank" href="https://www.linkedin.com/in/aurélie-runser">
             
                             <linkedinIcon class="inline-block w-7 h-7 md:w-10 md:h-10"/>
                             <p class="sr-only">Mon compte Linkedin : </p>
                             <p class="contact_link inline-block ml-3 w-auto font-darker-grotesque font-bold text-base md:text-lg text-orange-100 underline decoration-fuchsia-500">
-                                in/aurélie-runser-10a763224
+                                in/aurélie-runser
                             </p>
             
                         </a>
@@ -68,16 +68,22 @@
                 <form action="https://api.web3forms.com/submit" method="POST" ref="contactForm"
                     class="mx-5 my-14 md:ml-14 md:my-20 md:max-w-2xl xl:ml-28">
         
-        
-                    <input type="hidden" name="access_key" value="b3a5c0b1-dcc2-48e8-90e1-8ce766ba6253">
-        
+                    <input type="hidden" name="access_key" value="b3a5c0b1-dcc2-48e8-90e1-8ce766ba6253" autocomplete="off">
+
                     <div class="relative my-16">
-                        <input type="text" name="name" placeholder=" " required
-                                class="border-2 border-orange-100 p-2 md:p-5">
-                        <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
+
+                        <input
+                            type="text"
+                            name="prenom et nom"
+                            id="prenom et nom"
+                            placeholder=" "
+                            required
+                            class="border-2 border-orange-100 p-2 md:p-5">
+                        <label for="prenom et nom" 
+                                class="form_text absolute top-1/2 left-3 -translate-y-1/2
                                 font-darker-grotesque font-medium  text-orange-100 opacity-70">
                             Prénom & Nom
-                        </p>
+                        </label>
         
                         <div class="form_obligatoire p-3 md:p-5">
                             <p class="font-darker-grotesque font-bold text-sm 
@@ -86,12 +92,18 @@
                     </div>
         
                     <div class="relative my-16">
-                        <input type="email" name="email" placeholder=" " required
-                                class="border-2 border-orange-100 p-2 md:p-5">
-                        <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder=" "
+                            required
+                            class="border-2 border-orange-100 p-2 md:p-5">
+                        <label for="email"
+                               class="form_text absolute top-1/2 left-3 -translate-y-1/2
                                 font-darker-grotesque font-medium  text-orange-100 opacity-70">
                             Adresse Mail
-                        </p>
+                        </label>
         
                         <div class="form_obligatoire p-3 md:p-5">
                             <p class="font-darker-grotesque font-bold text-sm 
@@ -100,12 +112,18 @@
                     </div>
                     
                     <div class="relative my-16">
-                        <input type="text" name="sujet" placeholder=" " required
-                                class="border-2 border-orange-100 p-2 md:p-5">
-                        <p class="form_text absolute top-1/2 left-3 -translate-y-1/2
+                        <input
+                            type="text"
+                            name="sujet"
+                            id="sujet"
+                            placeholder=" "
+                            required
+                            class="border-2 border-orange-100 p-2 md:p-5">
+                        <label for="sujet"
+                                class="form_text absolute top-1/2 left-3 -translate-y-1/2
                                 font-darker-grotesque font-medium  text-orange-100 opacity-70">
                             Sujet
-                        </p>
+                        </label>
         
                         <div class="form_obligatoire p-3 md:p-5">
                             <p class="font-darker-grotesque font-bold text-sm 
@@ -114,13 +132,20 @@
                     </div>
         
                     <div class="relative my-16">
-                        <textarea name="message" rows="5" placeholder=" " required
-                        class="border-2 border-orange-100 p-2 md:p-5"></textarea>
-                        <p class="form_text absolute top-2 left-3
+                        <textarea
+                            name="message"
+                            id="message"
+                            rows="5"
+                            placeholder=" "
+                            required
+                            class="border-2 border-orange-100 p-2 md:p-5">
+                        </textarea>
+                        <label for="message"
+                                class="form_text absolute top-2 left-3
                                 font-darker-grotesque font-medium text-lg text-orange-100 opacity-70
                                 md:text-xl">
                             Message
-                        </p>
+                        </label>
         
                         <div class="form_obligatoire p-3 md:p-5">
                             <p class="font-darker-grotesque font-bold text-sm 
@@ -129,6 +154,42 @@
                     </div>
         
                     <!-- <input type="hidden" name="redirect" value="https://web3forms.com/success"> -->
+
+
+                    <!-- consentement RGPD -->
+                    <div class="relative my-16 flex items-start gap-5">
+
+                        <!-- CheckBox-->
+                        <input type="checkbox" name="consentementRGPD" required
+                                class="w-20 h-20 sm:w-10 sm:h-10 accent-cyan-400">
+
+                        <!-- Texte de consentement RGPD extrait -->
+                        <label class="inline-block font-darker-grotesque text-sm md:text-base text-stone-300"
+                                for="consentementRGPD">
+                            J'accepte que ces données personnelles soient collectées et utilisées par Aurélie Runser dans le but de répondre à mon message.
+                            <br/>Pour en savoir plus sur les condition d'utilisation de mes données,
+                            <button @click="RGPD = true"
+                                    class="contact_link inline-block font-darker-grotesque text-sm md:text-base text-orange-100 underline decoration-fuchsia-500">
+                                cliquez ici</button>.
+                        </label>
+
+
+                        <!-- Texte de consentement RGPD complet -->
+                        <div class="RGPDcomplet fixed z-50 p-5 md:p-10 bg-mon-black w-full md:w-3/4 top-1/2 left-1/2 -translate-x-1/2 translate-y-full"
+                            :aria-expanded="RGPD" :class="{ '-translate-y-[50%]': RGPD }">
+                        
+                            <p class="mx-auto w-fit font-darker-grotesque text-base xl:text-lg text-stone-300">
+                                En soumettant ce formulaire, vous consentez à ce que vos données personnelles, telles que votre prénom, votre nom, votre adresse e-mail et le contenu de votre message, soient collectées et utilisées par moi-même dans le but de répondre à votre demande de contact. Ces informations seront stockées en toute sécurité et ne seront pas partagées avec des tiers sans votre consentement préalable.
+    
+                                <br/>Vous pouvez à tout moment me demander de supprimer vos données personnelles de mes archives en m'envoyant un e-mail à aurelie.runser25@gmail.com.
+    
+                                <br/>En soumettant ce formulaire, vous donnez votre consentement libre, spécifique, éclairé et univoque au traitement de vos données personnelles conformément aux dispositions du Règlement Général sur la Protection des Données (RGPD).
+                            </p>   
+
+                            <monBouton class="mx-auto mt-5 " @click="RGPD = false">Fermer</monBouton>
+                        </div>
+
+                    </div>
         
                     <monBouton type="submit" class="mx-auto">
                         Envoyer
@@ -263,6 +324,30 @@ textarea:invalid ~ .form_obligatoire{
     border: 2px solid #d946ef;
 }
 
+
+
+/* style autour du message RGPD de l'envoie du formulaire */
+.RGPDcomplet::before{
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    display: block;
+    width: 101%;
+    height: 101%;
+    border: 2px solid #22d3ee;
+}
+.RGPDcomplet::after{
+    content: "";
+    position: absolute;
+    top: 0;
+    left : 0;
+    display: block;
+    width: 101%;
+    height: 101%;
+    border: 2px solid #d946ef;
+}
+
 </style>
 
 
@@ -279,7 +364,8 @@ export default {
 
   data() {
     return {
-      messageSent: false
+      messageSent: false,
+      RGPD: false,
     }
   },
 
