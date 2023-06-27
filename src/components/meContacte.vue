5f390e257bbca648640bc8e0d9283c37884e731e
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-06-27 19:15:29 +0200

    recpatcha correction

--- src/components/meContacte.vue
+++ src/components/meContacte.vue
@@ -384,7 +384,7 @@ export default {
 
   mounted(){
     const script = document.createElement('script');
-    script.src = 'https://www.google.com/recaptcha/api.js?render=' + this.recaptchaSiteKey;
+    script.src = 'https://www.google.com/recaptcha/api.js';
     script.async = true;
     script.defer = true;
     document.head.appendChild(script);
@@ -399,6 +399,7 @@ export default {
       const formData = new FormData(event.target);
       formData.append('recaptchaToken', token);
 
+        // if (grecaptcha.getResponse().length !== 0) {
       const response = await fetch('https://api.web3forms.com/submit', {
         method: 'POST',
         body: formData
