da6d3ad83c2a48f94172c0de593942a9d968c2ec
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-07-05 19:38:35 +0200

    404 : test

--- src/views/404.vue
+++ src/views/404.vue
@@ -2,6 +2,7 @@
     <div class="min-h-screen pt-24 overflow-hidden
     flex justify-between flex-col xl:flex-row">
 
+        <div>
             <h1 class="relative top-0 left-1/2 w-full animate-skew">
                 <span class="big-title-glitch btg-anim top-0 -translate-x-1/2 text-center text-5xl md:text-7xl"
                     data-text="Erreur 404">
@@ -25,6 +26,7 @@
                 <img class="hidden sm:block w-full max-w-md xl:hidden" src="/error.gif" alt="GIF où je cherche votre page dans le code">
                 
             </div>
+        </div>
         
         <div class="w-full hidden xl:flex xl:w-1/3 justify-end items-center my-10">
             <img class="w-full max-w-md xl:max-w-full" src="/error.gif" alt="GIF où je cherche votre page dans le code">
