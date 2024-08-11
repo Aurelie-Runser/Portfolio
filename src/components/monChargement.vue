74b99a51290b1d63329a1088c11c9676343fd7e7
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-08-11 17:34:35 +0200

    déplacement animation de chargement

---
+++ src/components/monChargement.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="w-full h-full relative">
+        <div class="log log_4 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-fuchsia-500"></div>
+        <div class="log log_3 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-stone-300"></div>
+        <div class="log log_2 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-cyan-400"></div>
+        <div class="log log_1 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-orange-100"></div>
+    </div>
+</template>
+
+<style>
+
+/* animation chargement carree beige */
+.log_1{
+    animation: log_1 5s ease-in-out infinite;
+}
+@keyframes log_1{
+    0%{
+        translate : -45px -45px
+    }
+    12.5%{
+        translate : 0
+    }
+    25%{
+        translate : 45px -45px
+    }
+    37.5%{
+        translate : 0
+    }
+    50%{
+        translate : 45px 45px
+    }
+    62.5%{
+        translate : 0
+    }
+    75%{
+        translate : -45px 45px
+    }
+    87.5%{
+        translate : 0
+    }
+    100%{
+        translate : -45px -45px
+    }
+}
+
+/* animation chargement carree bleu */
+.log_2{
+    animation: log_2 5s ease-in-out infinite;
+}
+@keyframes log_2{
+    0%{
+        translate : 45px -45px
+    }
+    12.5%{
+        translate : 0
+    }
+    25%{
+        translate : 45px 45px
+    }
+    37.5%{
+        translate : 0
+    }
+    50%{
+        translate : -45px 45px
+    }
+    62.5%{
+        translate : 0
+    }
+    75%{
+        translate : -45px -45px
+    }
+    87.5%{
+        translate : 0
+    }
+    100%{
+        translate : 45px -45px
+    }
+}
+
+/* animation chargement carree blanc */
+.log_3{
+    animation: log_3 5s ease-in-out infinite;
+}
+@keyframes log_3{
+    0%{
+        translate : 45px 45px
+    }
+    12.5%{
+        translate : 0
+    }
+    25%{
+        translate : -45px 45px
+    }
+    37.5%{
+        translate : 0
+    }
+    50%{
+        translate : -45px -45px
+    }
+    62.5%{
+        translate : 0
+    }
+    75%{
+        translate : 45px -45px
+    }
+    87.5%{
+        translate : 0
+    }
+    100%{
+        translate : 45px 45px
+    }
+}
+
+/* animation chargement carree rose */
+.log_4{
+    animation: log_4 5s ease-in-out infinite;
+}
+@keyframes log_4{
+    0%{
+        translate : -45px 45px
+    }
+    12.5%{
+        translate : 0
+    }
+    25%{
+        translate : -45px -45px
+    }
+    37.5%{
+        translate : 0
+    }
+    50%{
+        translate : 45px -45px
+    }
+    62.5%{
+        translate : 0
+    }
+    75%{
+        translate : 45px 45px
+    }
+    87.5%{
+        translate : 0
+    }
+    100%{
+        translate : -45px 45px
+    }
+}
+</style>
--- src/views/ProjetView.vue
+++ src/views/ProjetView.vue
@@ -127,12 +127,8 @@
         </div>
 
         <div v-else class="w-full h-screen relative">
-            <div class="log log_4 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-fuchsia-500"></div>
-            <div class="log log_3 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-stone-300"></div>
-            <div class="log log_2 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-cyan-400"></div>
-            <div class="log log_1 absolute top-1/3 left-1/2 w-10 h-10 -translate-x-1/2 -translate-y-1/2 bg-orange-100"></div>
+            <monChargement/>
         </div>
-
 
     </main>
 </template>
@@ -331,145 +327,6 @@
     }
 }
 
-
-/* chargement */
-
-/* animation chargement carree beige */
-.log_1{
-    animation: log_1 5s ease-in-out infinite;
-}
-@keyframes log_1{
-    0%{
-        translate : -45px -45px
-    }
-    12.5%{
-        translate : 0
-    }
-    25%{
-        translate : 45px -45px
-    }
-    37.5%{
-        translate : 0
-    }
-    50%{
-        translate : 45px 45px
-    }
-    62.5%{
-        translate : 0
-    }
-    75%{
-        translate : -45px 45px
-    }
-    87.5%{
-        translate : 0
-    }
-    100%{
-        translate : -45px -45px
-    }
-}
-
-/* animation chargement carree bleu */
-.log_2{
-    animation: log_2 5s ease-in-out infinite;
-}
-@keyframes log_2{
-    0%{
-        translate : 45px -45px
-    }
-    12.5%{
-        translate : 0
-    }
-    25%{
-        translate : 45px 45px
-    }
-    37.5%{
-        translate : 0
-    }
-    50%{
-        translate : -45px 45px
-    }
-    62.5%{
-        translate : 0
-    }
-    75%{
-        translate : -45px -45px
-    }
-    87.5%{
-        translate : 0
-    }
-    100%{
-        translate : 45px -45px
-    }
-}
-
-/* animation chargement carree blanc */
-.log_3{
-    animation: log_3 5s ease-in-out infinite;
-}
-@keyframes log_3{
-    0%{
-        translate : 45px 45px
-    }
-    12.5%{
-        translate : 0
-    }
-    25%{
-        translate : -45px 45px
-    }
-    37.5%{
-        translate : 0
-    }
-    50%{
-        translate : -45px -45px
-    }
-    62.5%{
-        translate : 0
-    }
-    75%{
-        translate : 45px -45px
-    }
-    87.5%{
-        translate : 0
-    }
-    100%{
-        translate : 45px 45px
-    }
-}
-
-/* animation chargement carree rose */
-.log_4{
-    animation: log_4 5s ease-in-out infinite;
-}
-@keyframes log_4{
-    0%{
-        translate : -45px 45px
-    }
-    12.5%{
-        translate : 0
-    }
-    25%{
-        translate : -45px -45px
-    }
-    37.5%{
-        translate : 0
-    }
-    50%{
-        translate : 45px -45px
-    }
-    62.5%{
-        translate : 0
-    }
-    75%{
-        translate : 45px 45px
-    }
-    87.5%{
-        translate : 0
-    }
-    100%{
-        translate : -45px 45px
-    }
-}
-
 </style>
 
 
@@ -477,6 +334,7 @@
 
 // import des composants
 import monBouton from "../components/monBouton.vue"
+import monChargement from "../components/monChargement.vue"
 
 // import des fonctions firebases
 import { 
@@ -506,7 +364,7 @@ import {
 
 export default {
     name:'ProjetView',
-    components: {monBouton},
+    components: {monBouton, monChargement},
 
     data() {
         return {
