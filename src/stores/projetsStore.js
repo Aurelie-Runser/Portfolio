b6c653af2f18afa008e6c1ac6f952b81d4c7bbac
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-08-12 18:42:05 +0200

    creation fichier firebase

--- src/stores/projetsStore.js
+++ src/stores/projetsStore.js
@@ -1,18 +1,12 @@
 import { defineStore } from 'pinia'
-
 import { 
-    getFirestore,   // Obtenir le Firestore
-    collection,     // Utiliser une collection de documents
-    onSnapshot,     // Demander une liste de documents d'une collection, en les synchronisant
-    orderBy,        // Permet de demander le tri d'une requête query
-    limit
-    } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js"
-
-import { 
-    getStorage,             // Obtenir le Cloud Storage
-    ref,                    // Pour créer une référence à un fichier à uploader
-    getDownloadURL,         // Permet de récupérer l'adress complète d'un fichier du Storage
-} from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js'
+    getFirestore, 
+    collection, 
+    onSnapshot, 
+    getStorage, 
+    ref, 
+    getDownloadURL 
+} from '@/stores/firebase.js';
 
 export const useProjetsStore = defineStore('projetsStore', {
     state: () => ({ 
