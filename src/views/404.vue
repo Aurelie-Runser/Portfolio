785e8aeb701ebcc5dd61b094594cec7e6ffd2ce7
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-08-18 01:24:18 +0200

    ajustement animation glitch

--- src/views/404.vue
+++ src/views/404.vue
@@ -19,7 +19,7 @@
                     </div>
                     
                     <RouterLink to="/">
-                        <monBouton class="mx-auto my-10">Retourner à Hello World</monBouton>
+                        <monBouton class="mx-auto my-10">Hello World</monBouton>
                     </RouterLink>
                 </div>
 
@@ -43,11 +43,9 @@
     display: block;
     position: absolute;
     top: 0;
-    left: -3px;
+    left: -2px;
     color: #ffedd5;
-    text-shadow: -5px 0 #d946ef;
-    background: #1f1f1f;
-    overflow: hidden;
+    text-shadow: -3px 0 #d946ef;
 }
 
 .btg-anim::after{
@@ -57,130 +55,10 @@
     top: 0;
     left: 0;
     color: #ffedd5;
-    text-shadow: -5px 0 #22d3ee;
+    text-shadow: 3px 0 #22d3ee;
     background: #1f1f1f;
-    overflow: hidden;
-    animation: glitch 1.5s linear infinite alternate-reverse;
-}
-
-@keyframes glitch {
-    5%{
-        clip-path: inset(48% 0 0 0);
-    } 
-    7.5%{
-        clip-path: inset(70% 0 0 0);
-    }
-    10%{
-        clip-path: inset(60% 0 0 0);
-    }
-    12.5%{
-        clip-path: inset(37% 0 0 0);
-    }
-    15%{
-        clip-path: inset(0 0 0 0);
-    }
-    17.5%{
-        clip-path: inset(0 0 40% 0);
-    }
-    20%{
-        clip-path: inset(0 0 40% 0);
-    }
-    22.5%{
-        clip-path: inset(0 0 60% 0);
-    }
-    25%{
-        clip-path: inset(0 0 30% 0);
-    }
-    27.5%{
-        clip-path: inset(0 0 50% 0);
-    }
-    30%{
-        clip-path: inset(40% 0 0 0);
-    }
-    32.5%{
-        clip-path: inset(33% 0 0 0);
-    } 
-    35%{
-        clip-path: inset(48% 0 0 0);
-    } 
-    37.5%{
-        clip-path: inset(70% 0 0 0);
-    }
-    40%{
-        clip-path: inset(60% 0 0 0);
-    }
-    42.5%{
-        clip-path: inset(37% 0 0 0);
-    }
-    45%{
-        clip-path: inset(0 0 0 0);
-    }
-    47.2%{
-        clip-path: inset(0 0 40% 0);
-    }
-    50%{
-        clip-path: inset(0 0 40% 0);
-    }
-    52.%{
-        clip-path: inset(33% 0 0 0);
-    } 
-    55%{
-        clip-path: inset(48% 0 0 0);
-    } 
-    57.5%{
-        clip-path: inset(70% 0 0 0);
-    }
-    60%{
-        clip-path: inset(60% 0 0 0);
-    }
-    62.5%{
-        clip-path: inset(37% 0 0 0);
-    }
-    65%{
-        clip-path: inset(0 0 0 0);
-    }
-    67.5%{
-        clip-path: inset(0 0 40% 0);
-    }
-    70%{
-        clip-path: inset(0 0 40% 0);
-    }
-    72.5%{
-        clip-path: inset(0 0 60% 0);
-    }
-    75%{
-        clip-path: inset(0 0 30% 0);
-    }
-    77.5%{
-        clip-path: inset(0 0 50% 0);
-    }
-    80%{
-        clip-path: inset(40% 0 0 0);
-    }
-    82.5%{
-        clip-path: inset(33% 0 0 0);
-    } 
-    85%{
-        clip-path: inset(48% 0 0 0);
-    } 
-    87.5%{
-        clip-path: inset(70% 0 0 0);
-    }
-    90%{
-        clip-path: inset(60% 0 0 0);
-    }
-    92.5%{
-        clip-path: inset(37% 0 0 0);
-    }
-    95%{
-        clip-path: inset(0 0 0 0);
-    }
-    97.5%{
-        clip-path: inset(0 0 40% 0);
-    }
-    100%{
-        clip-path: inset(0 0 40% 0);
-    }
+    clip-path: inset(62% 0% 0% 0%);
+    animation: glitch 3s linear infinite alternate;
 }
 
 </style>
